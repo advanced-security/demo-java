6e1a8c450e048c1f6655d738cec630c473ab6a82
Tony Cheung <tonycch@github.com>
2021-05-10 17:44:00 +0800

    first commit

---
+++ src/main/java/com/github/hackathon/advancedsecurityjava/AdvancedSecurityJavaApplication.java
@@ -0,0 +1,13 @@
+package com.github.hackathon.advancedsecurityjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedSecurityJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedSecurityJavaApplication.class, args);
+	}
+
+}
